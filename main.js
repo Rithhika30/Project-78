1c3c0df6b611f3e83aed6d04d0f5e9a5261bba5f
Rithhika30 <81682417+Rithhika30@users.noreply.github.com>
2021-08-31 09:37:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var images = ["https://as2.ftcdn.net/jpg/01/15/47/87/500_F_115478791_GG0yIcVA8Mhe6MAXEvgcfyL7JtuVfYvm.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg", "https://previews.123rf.com/images/yupiramos/yupiramos1712/yupiramos171207137/91230755-young-girl-kid-character-standing-cartoon-vector-illustration.jpg", "https://previews.123rf.com/images/yupiramos/yupiramos1712/yupiramos171207137/91230755-young-girl-kid-character-standing-cartoon-vector-illustration.jpg", "download.jpg"];
+var names = ["Mom", "Dad", "Me", "Little Sister", "Us"];
+var i = 0;
+
+function update() {
+
+  var array_length = images.length - 1;
+  var updated_image = images[i];
+  var updated_text = names[i];
+
+  document.getElementById("family_book_image").src = updated_image;
+  document.getElementById("name").innerHTML = updated_text;
+
+  i++;
+
+
+  if (i > array_length) {
+    i = 0;
+
+  }
+}
